eecadd86a00c13098171043af48620ce1e899cad
Guillaume Simon <guillaumesimon597@gmail.com>
2024-10-23 11:46:38 +0200

    Ajout intercepteur / requête

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import {
 } from '@ionic/angular/standalone';
 import { AppComponent } from './app/app.component';
 import {
+  HTTP_INTERCEPTORS,
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
@@ -21,6 +22,10 @@ import {
   MsalBroadcastService,
   MSAL_GUARD_CONFIG,
   MsalGuardConfiguration,
+  MsalInterceptor,
+  MsalRedirectComponent,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalInterceptorConfiguration,
 } from '@azure/msal-angular';
 import { PublicClientApplication, InteractionType } from '@azure/msal-browser';
 import { routes } from './app/app.routes';
@@ -48,22 +53,33 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
   return {
     interactionType: InteractionType.Redirect, // Use redirect for authentication
     authRequest: {
-      scopes: ['user.read'], // Define the required scopes
+      scopes: ['openid profile email offline_access api://b9385f89-d371-4a84-8126-4ae9fb977898/profil'], // Define the required scopes
     },
   };
 }
-
+export function MsalInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap: new Map([
+      ['http://localhost:4200', ['api://b9385f89-d371-4a84-8126-4ae9fb977898/profil']],
+    ])
+  };
+}
 // Bootstrap the application with MSAL and other providers
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: MSAL_INSTANCE, useFactory: MSALInstanceFactory }, // Provide the MSAL instance
     { provide: MSAL_GUARD_CONFIG, useFactory: MSALGuardConfigFactory }, // Provide the MSAL Guard configuration
+    { provide: MSAL_INTERCEPTOR_CONFIG, useFactory: MsalInterceptorConfigFactory }, // Configuration de l'intercepteur
+    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true },
     MsalService,
     MsalGuard,
     MsalBroadcastService,
+    MsalRedirectComponent,
+
     provideHttpClient(withInterceptorsFromDi()), // Provide HttpClient with interceptors
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-  ],
+  ]
 });
