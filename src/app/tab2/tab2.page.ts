f5f9c555bcd77a29f7837bda497b5e96b0e5cafa
VivienS5 <92254790+VivienS5@users.noreply.github.com>
2024-10-24 10:07:26 +0200

    Branchaudioback (#7)
    * init
    
    * Ajustement page lecture livre
    
    ---------
    
    Co-authored-by: TorresDaCostaTheo <newtete789@gmail.com>

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
@@ -15,6 +21,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { pause } from 'ionicons/icons';
 import { Book } from '../models/Book';
 import { MsalService } from '@azure/msal-angular';
+import { AudioService } from '../service/AudioService';
 
 @Component({
   selector: 'app-tab2',
@@ -45,25 +52,47 @@ export class Tab2Page implements OnInit {
     author: 'waiting',
     chapters: [],
   };
+
+
   @ViewChild('audioPlayer') audioPlayerRef!: ElementRef<HTMLAudioElement>;
-  audioSrc = 'assets/audio/VillagoisMinecraftTaPeur.mp3';
+  audioSrc = ''; // Pas de fichier local 
   isPlaying = false;
   currentTime = '0:00';
   duration = '0:00';
   progress = 0;
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private msalService: MsalService
-  ) {}
+
+  // Utilisation de inject() pour l'injection des services
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private msalService = inject(MsalService);
+  private audioService = inject(AudioService); // Injection directe du service
+
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
       if (params['data']) {
         this.receivedBook = JSON.parse(params['data']);
         console.log(this.receivedBook); // Affiche le livre passé
+        this.loadAudio(); // Charger l'audio dès qu'on a le livre
       }
     });
   }
+
+  loadAudio() {
+    // Appel de l'API pour récupérer l'audio du livre en fonction de son ID
+    this.audioService.getAudioByBookId(this.receivedBook.idBook).subscribe(
+      (audioBlob) => {
+        // Créer une URL à partir du Blob reçu (fichier audio)
+        const audioURL = URL.createObjectURL(audioBlob);
+        this.audioSrc = audioURL; // Assigner l'URL à la source audio
+        console.log('Audio chargé avec succès', this.audioSrc);
+      },
+      (error) => {
+        console.error('Erreur lors du chargement de l\'audio', error);
+      }
+    );
+  }
+  
+
   togglePlayPause() {
     const audio = this.audioPlayerRef.nativeElement;
     if (this.isPlaying) {
@@ -100,9 +129,11 @@ export class Tab2Page implements OnInit {
     const secs = Math.floor(seconds % 60);
     return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
   }
+
   goToHome() {
     this.router.navigate(['/']);
   }
+
   getUser() {
     const account = this.msalService.instance.getActiveAccount();
     console.log('User Info:', account);
